693d1f0d689bba7d30efd616d0b4c4bfe2f776c1
vivekdabral <38310199+vivekdabral@users.noreply.github.com>
2022-04-11 20:25:42 +1000

    remvoed file

--- main.tf
+++ main.tf
@@ -39,8 +39,25 @@ resource "aws_elb" "bar" {
 
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "web" {
   instance_type = "t3.micro"
+    ami          = data.aws_ami.ubuntu.id
 
   tags = {
     Name = "NameInstance"
